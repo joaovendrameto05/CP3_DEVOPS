0e601a297dee37a96cd7a828060fdfbc52b9e877
joao vendrameto <joaovendrameto5mh@gmail.com>
2026-05-18 22:03:42 -0300

    Refactor LancamentoController: reorganize imports and enhance code structure

--- src/main/java/com/dimdim/api/controller/LancamentoController.java
+++ src/main/java/com/dimdim/api/controller/LancamentoController.java
@@ -1,9 +1,31 @@
 package com.dimdim.api.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 import com.dimdim.api.model.Lancamento;
 import com.dimdim.api.repository.LancamentoRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
+@RestController
+@RequestMapping("/lancamentos")
+public class LancamentoController {
+
+    @Autowired
+    private LancamentoRepository repository;
+
+    @GetMapping
+    public List<Lancamento> listar() {
+        return repository.findAll();
+    }
+
+    @PostMapping
+    public Lancamento criar(@RequestBody Lancamento lancamento) {
+        return repository.save(lancamento);
+    }
+}
