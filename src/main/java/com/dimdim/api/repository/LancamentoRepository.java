72f3696b931501201085e83c882eabf6e6ddfdde
joaovendrameto05 <joaovendrameto5mh@gmail.com>
2026-05-18 21:04:27 -0300

    Remove duplicate import statement for Lancamento

--- src/main/java/com/dimdim/api/repository/LancamentoRepository.java
+++ src/main/java/com/dimdim/api/repository/LancamentoRepository.java
@@ -1,9 +1,8 @@
 package com.dimdim.api.repository;
 
+import com.dimdim.api.model.Lancamento;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
-import com.dimdim.api.model.Lancamento;
 
 @Repository
 public interface LancamentoRepository extends JpaRepository<Lancamento, Long> {
